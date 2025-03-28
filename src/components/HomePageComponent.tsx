f5e1ea8e4326b5689b1ce23df096c85c01940593
0x52afael <rafael.dalesandro@hotmail.com>
2025-03-28 20:09:13 +0100

    Working on NAV bar

--- src/components/HomePageComponent.tsx
+++ src/components/HomePageComponent.tsx
@@ -7,14 +7,14 @@ type Props = {};
 function HomePageComponent(props: Props) {
 	return (
 		<body className="home ">
-			<div className="text-white" id="home-page">
+			<div className="text-white">
 				<div>
 					<Navigation
 						content={["00 Home", "01 Destination", "02 Crew", "03 Technology"]}
 						hrefs={["#", "#interactive-elements", "#endregion"]}
 					/>
 				</div>
-				<div className="grid-container grid-container--home screen-size-70">
+				<div className="grid-container grid-container--home ">
 					<div>
 						<h1 className="flex flex-d-column ff-sans-cond letter-spacing-1 fs-500 uppercase text-accent">
 							So, you want to travel to{" "}
@@ -22,12 +22,11 @@ function HomePageComponent(props: Props) {
 								Space
 							</span>
 						</h1>
-						<p className="text-accent">
-							{" "}
+						<p className="text-accent ">
 							Let’s face it; if you want to go to space, you might as well
 							genuinely go to outer space and not hover kind of on the edge of
 							it. Well sit back, and relax because we’ll give you a truly out of
-							this world experience !{" "}
+							this world experience !
 						</p>
 					</div>
 					<div>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,41 +1,53 @@
 import React from "react";
 
 interface NavigationProps {
-    content: string[],
-    hrefs: string[],
+	content: string[];
+	hrefs: string[];
 }
 
-
-const navigationContentHelper = (content: string[], hrefs: string[]): JSX.Element[] => {
-    return content.map((tabItem, idx) => {
-
-        const tabItemContent: string[] = tabItem.split(" ");
-        return (
-            <li>
-                <a className="text-white letter-spacing-1" href={hrefs?.[idx]} tabIndex={0}>
-                    <span>{tabItemContent?.[0]}</span>
-                    {tabItemContent?.[1]}
-                </a>
-            </li>)
-    })
-}
-
-
-function Navigation(props: NavigationProps) {
-
-    return (
-        <div className="nav-container flex w-full flex-center">
-            <div className="logo flex" />
-
-            <div className="navigation-line"></div>
-            <nav>
-                <ul className="primary-navigation underline-indicators flex ff-sans-cond uppercase flex-center">
-                    {navigationContentHelper(props.content, props.hrefs)}
-                </ul>
-            </nav>
-        </div>
-    );
+const navigationContentHelper = (
+	content: string[],
+	hrefs: string[],
+): JSX.Element[] => {
+	return content.map((tabItem, idx) => {
+		const tabItemContent: string[] = tabItem.split(" ");
+		return (
+			<li>
+				<a
+					className="text-white letter-spacing-1 fs-400"
+					href={hrefs?.[idx]}
+					tabIndex={0}
+				>
+					<span>{tabItemContent?.[0]}</span>
+					{tabItemContent?.[1]}
+				</a>
+			</li>
+		);
+	});
 };
 
+function Navigation(props: NavigationProps) {
+	return (
+		<div className="nav-container flex flex-center">
+			<div className="logo " />
+
+			<button className="bg-transparent hamburger">
+				<span
+					className="sr-only mobile-nav-toggle"
+					aria-controls="primary-navigation"
+					aria-expanded="false"
+				>
+					Menu
+				</span>
+			</button>
+
+			<nav className=" mx-5">
+				<ul className="primary-navigation underline-indicators flex  ff-sans-cond uppercase ">
+					{navigationContentHelper(props.content, props.hrefs)}
+				</ul>
+			</nav>
+		</div>
+	);
+}
 
 export default Navigation;
